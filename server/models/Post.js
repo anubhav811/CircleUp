e0be5fde1fdfb6735a92ddfcb902d6e018349590
Anubhav Chachra <chachraanubhav@gmail.com>
2023-07-10 17:32:10 +0530

    added dummy data to db

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
-import express from "express";
 
-const postSchema = new mongoose.Schema({
+const postSchema = mongoose.Schema(
+  {
     userId: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    lastName:{
-        type: String,
-        required: true,
+    lastName: {
+      type: String,
+      required: true,
     },
-    location:String,
-    description:String,
+    location: String,
+    description: String,
     picturePath: String,
-    userPicturePath: String,    
+    userPicturePath: String,
     likes: {
-        type: Map,
-        of: Boolean,
+      type: Map,
+      of: Boolean,
     },
     comments: {
-        type: Map,
-        default : []
+      type: Array,
+      default: [],
     },
-}, 
-{timestamps: true}
-
+  },
+  { timestamps: true }
 );
 
-export default mongoose.model("Post",postSchema);
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
